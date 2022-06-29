1dc659df744982b2979b1bab9b108d27a2e9558b
Gian Manzo <ggianmanzo@gmail.com>
2022-06-29 18:27:07 -0300

    add page product, add fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
 import { Products } from './Pages/Products'
 import { Contact } from './Pages/Contact'
+import { Product } from './Pages/Product'
 
 const AppContainer = styled.div``
-
 const AppContent = styled.div``
 
 export const App = () => {
@@ -19,6 +19,7 @@ export const App = () => {
         <AppContent>
           <Routes>
             <Route path="/" element={<Products />} />
+            <Route path="product/:id" element={<Product />} />
             <Route path="contact" element={<Contact />} />
           </Routes>
         </AppContent>
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
 
-const ProductsSection = styled.section``
+const ProductsSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+`
+
+const PoductName = styled.h1`
+  margin: 0.5rem 0;
+`
 
 export const Products = () => {
+  const [products, setProducts] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then(r => r.json())
+      .then(json => setProducts(json))
+  }, [])
+
+  if (products === null) return null
   return (
-    <ProductsSection>
+    <ProductsSection className="animeleft">
       <Helmet>
         <title>React | Produtos</title>
         <meta name="description" content="" />
       </Helmet>
-      <h1>Produtos</h1>
+
+      {products.map(product => (
+        <Link to={`product/${product.id}`} key={product.id}>
+          <img src={product.fotos[0].src} alt={product.fotos[0].titulo} />
+          <PoductName>{product.nome}</PoductName>
+        </Link>
+      ))}
     </ProductsSection>
   )
 }
